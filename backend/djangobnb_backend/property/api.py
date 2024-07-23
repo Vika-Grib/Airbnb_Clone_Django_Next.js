6efcde1bf558d90881d0350045904193cef1231c
Victoria Grib <130666532+Vika-Grib@users.noreply.github.com>
2024-07-23 20:10:05 +0200

    all project is done

--- backend/djangobnb_backend/property/api.py
+++ backend/djangobnb_backend/property/api.py
@@ -7,6 +7,7 @@ from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
 from useraccount.models import User
 
+
 @api_view(['GET'])
 @authentication_classes([])
 @permission_classes([])
@@ -22,32 +23,68 @@ def properties_list(request):
     except Exception as e:
         user = None
 
-    print('user!', user)
+    #
+    #
 
-    #
-    #
     favorites = []
     properties = Property.objects.all()
 
     #
+    # Filter
+
     is_favorites = request.GET.get('is_favorites', '')
-    # фильтр для landlord, объявления которые у владельца
     landlord_id = request.GET.get('landlord_id', '')
 
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkIn', '')
+    checkout_date = request.GET.get('checkOut', '')
+    bedrooms = request.GET.get('numBedrooms', '')
+    guests = request.GET.get('numGuests', '')
+    bathrooms = request.GET.get('numBathrooms', '')
 
-    #  тут в нащ properties мы добавляем то, что в фильтре
+    print('country', country)
+
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(
+            end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkout_date, end_date__gte=checkin_date)
+        all_matches = []
+
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property_id)
+
+        properties = properties.exclude(id__in=all_matches)
+
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
 
     if is_favorites:
         properties = properties.filter(favorited__in=[user])
 
+    if guests:
+        properties = properties.filter(guests__gte=guests)
+
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+
+    if country:
+        properties = properties.filter(country=country)
+
+    if category and category != 'undefined':
+        properties = properties.filter(category=category)
+
     #
     # Favorites
+
     if user:
         for property in properties:
             if user in property.favorited.all():
                 favorites.append(property.id)
+
     #
     #
 
@@ -90,12 +127,11 @@ def create_property(request):
         property = form.save(commit=False)
         property.landlord = request.user
         property.save()
-        return JsonResponse({'success': True})
+
         return JsonResponse({'success': True})
     else:
-        print('error:', form.errors, form.non_field_errors())
-        return JsonResponse({'errors': form.errors}, status=400)
-
+        print('error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status=400)
 
 
 @api_view(['POST'])
@@ -124,6 +160,7 @@ def book_property(request, pk):
         print('Error', e)
 
         return JsonResponse({'success': False})
+
 
 @api_view(['POST'])
 def toggle_favorite(request, pk):
